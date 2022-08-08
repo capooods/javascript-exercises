0cada08c1947799f5187b52afde82e92961b4569
capooods <carlo.d.capio@gmail.com>
2022-08-08 16:51:29 +0800

    Complete activity 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    let word = "";
+    if (repeat>=0) {
+        for (let i=0; i<repeat; i++) {
+            word = word + string;
+        }
+        return word;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
