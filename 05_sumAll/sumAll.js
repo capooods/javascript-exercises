8771df219badb1434f9131f88e57c4e4f0fe6b26
capooods <carlo.d.capio@gmail.com>
2022-08-08 17:24:10 +0800

    Complete activity 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(initial, final) {
+    if ((initial >= 0 && final >= 0) && (typeof initial === "number" && typeof final === "number")) {
+        let total = 0;
+        if (initial <= final) {
+            for (i=initial; i<=final; i++) {
+                total = total + i;
+            }
+            return total;
+        } else {
+            for (i=final; i<=initial; i++) {
+                total = total + i;
+            }
+            return total;
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
