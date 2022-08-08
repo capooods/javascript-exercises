5b02a6d787d0675b67318d923b078b3bf1c11f45
capooods <carlo.d.capio@gmail.com>
2022-08-08 17:15:01 +0800

    Complete activity 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, numRemove) {
+    for (let i=1; i<=arguments.length+1; i++) {
+        let index = givenArray.indexOf(arguments[i]);
+        if (index > -1) {
+            givenArray.splice(index, 1);
+        }
+    }
+    
+    return givenArray;
 };
 
 // Do not edit below this line
